77f108551ad2d56128d65d8716b063f3cea6bd89
Paul Dickens <dickens.paul@gmail.com>
2018-08-10 22:34:17 +1000

    better messages

--- src/app/transmitter.service.ts
+++ src/app/transmitter.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Transmitter } from './transmitter';
 import { TRANSMITTERS } from './mock-transmitters';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransmitterService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getTransmitters(): Observable<Transmitter[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('TransmitterService: fetched transmitters');
     return of(TRANSMITTERS);
   }
 }
