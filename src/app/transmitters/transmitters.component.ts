c0aa29055b3cb0c0d943b479c5120d00e9d462fe
Paul Dickens <dickens.paul@gmail.com>
2018-08-20 08:42:59 +1000

    observable table

--- src/app/transmitters/transmitters.component.ts
+++ src/app/transmitters/transmitters.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataSource } from '@angular/cdk/collections';
 import { Transmitter } from '../transmitter';
 import { TransmitterService } from '../transmitter.service';
 import { TimeAgoPipe } from 'time-ago-pipe';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-transmitters',
@@ -13,6 +35,14 @@ export class TransmittersComponent implements OnInit {
   date: Date = new Date('2018-06-21T12:47:06.704Z');
   connection;
 
+  // displayedColumns: string[] = ['name', 'email', 'phone', 'company'];
+  displayedColumns: string[] = ['id', 'readDate', 'activationDate', 'rssi'];
+  dataSource = new TransmitterDataSource(this.transmitterService);
+
+
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // dataSource = ELEMENT_DATA;
+
   constructor(private transmitterService: TransmitterService) { }
 
   ngOnInit() {
@@ -21,7 +51,7 @@ export class TransmittersComponent implements OnInit {
       console.log('got message');
       this.getTransmitters(); // TODO: this is overkill
     })
-  }
+  }fsfewt
 
   ngOnDestroy() {
     this.connection.unsubscribe();
@@ -47,3 +77,18 @@ export class TransmittersComponent implements OnInit {
     this.transmitterService.deleteTransmitter(transmitter).subscribe();
   }
 }
+export class TransmitterDataSource extends DataSource<any> {
+  constructor(private transmitterService: TransmitterService) {
+    super();
+  }
+  connect(): Observable<Transmitter[]> {
+    return this.transmitterService.getTransmitters();
+  }
+  disconnect() {}
+}
+
+
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/observable/of';
+// import {DataSource} from '@angular/cdk/collections';
+// import { User } from '../../models/user.model';
