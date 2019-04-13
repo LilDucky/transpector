8c42aa97f1d5fe0a79409498ee39f3585a09065b
Paul Dickens <dickens.paul@gmail.com>
2019-04-13 22:01:51 +1000

    fix dist path

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var app = express();
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist/transmitter-inspector/";
 app.use(express.static(distDir));
 
 // // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
